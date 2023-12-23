9a8dc131dc1b5cb768cb91dad985eb752d9fd81b
Ethan Cratchley <138141942+EthanCratchley@users.noreply.github.com>
2023-12-23 11:51:04 -0800

    summarization working

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from rss.parseRSS import parse_rss
 from rss.fetchRSS import fetch_full_article
+from summarize import summarize_article
+
+
+from bs4 import BeautifulSoup
 import requests 
 import feedparser
-from bs4 import BeautifulSoup
 
-def fetch_full_article(article_url):
+def fetch_full_article(article_url, max_paragraphs=5):
     """
-    Fetches the full article content from a given URL.
+    Fetches and truncates the full article content from a given URL.
+    max_paragraphs: Maximum number of paragraphs to include in the summary.
     """
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
@@ -16,8 +19,10 @@ def fetch_full_article(article_url):
         response = requests.get(article_url, headers=headers)
         soup = BeautifulSoup(response.content, 'html.parser')
         article_body = soup.find_all(['p', 'h1', 'h2', 'h3'])
-        full_text = ' '.join([para.get_text() for para in article_body])
-        return full_text
+
+        # Truncate the article to the specified number of paragraphs
+        truncated_article = ' '.join([para.get_text() for para in article_body[:max_paragraphs]])
+        return truncated_article
     except Exception as e:
         return f"Error fetching full article: {e}"
 
@@ -39,8 +44,9 @@ def get_one_article(rss_url):
             author = item.get('author', 'No author available')
 
             full_article = fetch_full_article(link)
+            summary = summarize_article(full_article)
 
-            print(f"Title: {title}\nLink: {link}\nDescription: {description}\nDate: {pub_date}\nAuthor: {author}\nFull Article: {full_article}")
+            print(f"Title: {title}\nLink: {link}\nDescription: {description}\nDate: {pub_date}\nAuthor: {author}\nSummary: {summary}")
         else:
             print("No items found in the RSS feed.")
     else:
@@ -48,7 +54,8 @@ def get_one_article(rss_url):
 
 if __name__ == "__main__":
     # Example RSS URL
-    rss_url = 'http://rss.cnn.com/rss/money_news_economy.rss'
+    rss_url = 'link'
 
     # Process the article from the feed
     get_one_article(rss_url)
+
--- summarize.py
+++ summarize.py
@@ -1,100 +1,17 @@
-import json
+from openai import OpenAI
 
-import openai
-import tiktoken
-
-
-def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
-    """Return the number of tokens used by a list of messages."""
-    try:
-        encoding = tiktoken.encoding_for_model(model)
-    except KeyError:
-        print("Warning: model not found. Using cl100k_base encoding.")
-        encoding = tiktoken.get_encoding("cl100k_base")
-    if model in {
-        "gpt-3.5-turbo-0613",
-        "gpt-3.5-turbo-16k-0613",
-        "gpt-4-0314",
-        "gpt-4-32k-0314",
-        "gpt-4-0613",
-        "gpt-4-32k-0613",
-    }:
-        tokens_per_message = 3
-        tokens_per_name = 1
-    elif model == "gpt-3.5-turbo-0301":
-        tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
-        tokens_per_name = -1  # if there's a name, the role is omitted
-    elif "gpt-3.5-turbo" in model:
-        # print("Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613.")
-        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613")
-    elif "gpt-4" in model:
-        # print("Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.")
-        return num_tokens_from_messages(messages, model="gpt-4-0613")
-    else:
-        raise NotImplementedError(
-            f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
-        )
-    num_tokens = 0
-    for message in messages:
-        num_tokens += tokens_per_message
-        for key, value in message.items():
-            num_tokens += len(encoding.encode(value, disallowed_special=()))
-            if key == "name":
-                num_tokens += tokens_per_name
-    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
-    return num_tokens
-
-def get_post_insights(title, fullText, model="gpt-3.5-turbo"):
-    prompt = f"Summarize this article in one to two sentences based on the title and full text I provide you. Title: '{title}' Full Text: '{fullText}'"
-    
-    messages = [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": prompt}
-    ]
-    
+def summarize_article(article_text):
     """
-def get_post_insights(title, fullText, model="gpt-3.5-turbo"):
-    # Set maximum number of tokens
-    max_tokens = 4096
+    Summarizes the given article text using OpenAI's GPT model.
+    """
+    client = OpenAI(api_key="key")  # Replace 'YOUR_API_KEY' with your actual API key
 
-    # Create a combined prompt for summary and buzzwords
-    prompt = f"Summarize this article in one to two sentences based on the title and full text I provide you. Also, give me a list of the top 5 most important buzzwords from the same. Respond only in JSON, using 'summary' and 'buzzwords' as the keys. Do not say what you're describing, i.e. don't start with 'this blogpost is about'.\n\nTitle: '{title}'\n\nFull Text: '{fullText}'"
-
-    messages = [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": prompt},
-    ]
     try:
-        completion = openai.ChatCompletion.create(model=model, messages=messages)
-        content = completion.choices[0].message["content"]
-        # Parse JSON response here as needed
-        return content  # Or return a parsed version of the response
+        response = client.completions.create(
+            model="text-davinci-003",  # or another model you prefer
+            prompt=f"Summarize this article:\n\n{article_text}",
+            max_tokens=3897  # Adjust based on your needs
+        )
+        return response.choices[0].text.strip()
     except Exception as e:
-        print(f"Error in API call: {e}")
-        return "No summary generated due to API error."
-
-    # Get the number of tokens for the message
-    num_tokens = num_tokens_from_messages(messages, model=model)
-
-    # Shorten the fullText if the total number of tokens exceeds max_tokens
-    while num_tokens > max_tokens:
-        print("Shortening fullText")
-        fullText = fullText[: int(len(fullText) * 0.9)]
-        prompt = f"Summarize this article in one to two sentences based on the title and full text I provide you. Also, give me a list of the top 5 most important buzzwords from the same. Respond only in JSON, using 'summary' and 'buzzwords' as the keys. Do not say what you're describing, i.e. don't start with 'this blogpost is about'.\n\nTitle: '{title}'\n\nFull Text: '{fullText}'"
-        messages[-1]["content"] = prompt
-        num_tokens = num_tokens_from_messages(messages, model=model)
-
-    # Obtain completion for the prompt
-    completion = openai.ChatCompletion.create(model=model, messages=messages)
-
-    # Extract results
-    content = completion.choices[0].message["content"]
-    try:
-        result = json.loads(content)
-    except json.decoder.JSONDecodeError:
-        return {"summary": "No summary generated.", "buzzwords": []}
-    
-    summary = result.get("summary", "No summary generated.")
-    buzzwords = result.get("buzzwords", [])
-    return {"summary": summary, "buzzwords": buzzwords}
-"""
+        return f"Error in summarization: {e}"
