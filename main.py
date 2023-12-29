a760edb963b12c121e81b2da68142d4f860d874e
Ethan Cratchley <138141942+EthanCratchley@users.noreply.github.com>
2023-12-29 11:27:10 -0800

    updates

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def fetch_full_article(article_url, max_paragraphs=5):
     except Exception as e:
         return f"Error fetching full article: {e}"
 
-def get_multiple_articles(rss_url, number_of_articles=3):
+def get_multiple_articles(rss_url, number_of_articles=1):
     """
     Fetches multiple articles from an RSS feed.
     number_of_articles: Number of articles to process from the feed.
@@ -52,9 +52,26 @@ def get_multiple_articles(rss_url, number_of_articles=3):
         print(f"Failed to fetch RSS feed. HTTP Status Code: {response.status_code}")
 
 if __name__ == "__main__":
-    # Example RSS URL
-    rss_url = 'https://www.frbsf.org/our-district/about/sf-fed-blog-rss-feed/'
+    # List of example RSS URLs
+    rss_urls = [
+        #Crypto
+        'https://Blockchain.News/RSS/',
+        'https://bitcoinist.com/feed/',
+        'https://www.newsbtc.com/feed/',
+        # Stock Market Movements
+        'https://www.reutersagency.com/feed/?best-topics=business-finance&post_type=best',
+        'https://seekingalpha.com/feed.xml',
+        'https://feeds.a.dj.com/rss/RSSMarketsMain.xml',
+        'https://www.frbsf.org/our-district/about/sf-fed-blog-rss-feed/',
+        # Economic Indicators
+        'https://tradingeconomics.com/canada/rss',
+        'https://tradingeconomics.com/united-states/rss',
+        'https://feeds.content.dowjones.io/public/rss/mw_topstories',
+        'https://api.io.canada.ca/io-server/gc/news/en/v2?dept=departmentfinance&type=newsreleases&sort=publishedDate&orderBy=desc&publishedDate%3E=2020-08-09&pick=100&format=atom&atomtitle=Canada%20News%20Centre%20-%20Department%20of%20Finance%20Canada%20-%20News%20Releases'
+    ]
 
-    # Process the article from the feed
-    get_multiple_articles(rss_url)
-
+    # Process articles from each feed
+    for rss_url in rss_urls:
+        print(f"Fetching articles from: {rss_url}\n")
+        get_multiple_articles(rss_url)
+        print("\n-----\n")
