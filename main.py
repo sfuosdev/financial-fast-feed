9cc5cd0a7d01cbe244af5f190edeb509f2250a22
Ethan Cratchley <138141942+EthanCratchley@users.noreply.github.com>
2024-08-08 10:42:08 -0700

    fix

--- main.py
+++ main.py
@@ -105,14 +105,12 @@ def main():
         # Stock Market Movements
         'https://www.reutersagency.com/feed/?best-topics=business-finance&post_type=best',
         'https://seekingalpha.com/feed.xml',
-        'https://feeds.a.dj.com/rss/RSSMarketsMain.xml',
         'https://www.frbsf.org/our-district/about/sf-fed-blog-rss-feed/',
         'https://fortune.com/feed/fortune-feeds/?id=3230629',
 
         # Economic Indicators
         'https://tradingeconomics.com/canada/rss',
         'https://tradingeconomics.com/united-states/rss',
-        'https://feeds.content.dowjones.io/public/rss/mw_topstories',
         'https://api.io.canada.ca/io-server/gc/news/en/v2?dept=departmentfinance&type=newsreleases&sort=publishedDate&orderBy=desc&publishedDate%3E=2020-08-09&pick=100&format=atom&atomtitle=Canada%20News%20Centre%20-%20Department%20of%20Finance%20Canada%20-%20News%20Releases'
     ]
 
